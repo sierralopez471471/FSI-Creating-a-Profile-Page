89014b6bc59187fd4b0a02939edd8459e3960929
Sierra Lopez <sierralopez1006@gmail.com>
2022-05-16 21:07:29 -0700

    emulating html script using Javascript

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 let content = document.querySelector('.js-generated.content')
+let header = document.createElement('h1')
+header.setAttribute('class', 'dog-name')
+header.append('Rizzo')
+content.append(header)
+
+let div = document.createElement('div')
+div.setAttribute('class', 'dog-content')
+content.append(div)
+
+let img = document.createElement('img')
+img.setAttribute('class', 'dog-image')
+img.setAttribute('src', './assets/rizzo.jpg')
+div.append(img)
+
+let dogDetails = document.createElement('div')
+dogDetails.setAttribute('class', 'dog-details')
+div.append(dogDetails)
+let h3 = document.createElement('h3')
+h3.append('Description: ')
+
+div.append(h3)
